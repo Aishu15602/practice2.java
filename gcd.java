65bf757968562645043ade28f1a4349352afcf1d
Aishu15602 <aiswarya06211@gmail.com>
2025-10-06 14:30:03 +0530

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class gcd {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int n1 = input.nextInt(), n2 = sc.nextInt();
+		int n1 = input.nextInt(), n2 = input.nextInt();
 		int dividend = (n1 > n2) ? n1 : n2;
 		int divisor = (n1 < n2) ? n1 : n2;
 		int rem = dividend % divisor;
